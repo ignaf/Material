4105126cb53ac6b441be6e0f75d867cf51f66e26
ignaf <ignaa88@gmail.com>
2022-05-02 15:35:43 -0300

    practica clase layout

---
+++ Clase-Layouts-PartialViews-Validaciones/Clase-Layouts-PV-Valid/Clase-Layouts-PV-Valid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clase_Layouts_PV_Valid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
