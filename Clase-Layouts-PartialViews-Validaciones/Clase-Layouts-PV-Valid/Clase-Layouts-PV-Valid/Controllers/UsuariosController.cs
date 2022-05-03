4c43078f9045dbb39d4d2e548a45cf1066b74e29
ignaf <ignaa88@gmail.com>
2022-05-03 00:31:10 -0300

    agregando form usuario y lista

---
+++ Clase-Layouts-PartialViews-Validaciones/Clase-Layouts-PV-Valid/Clase-Layouts-PV-Valid/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using Clase_Layouts_PV_Valid.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase_Layouts_PV_Valid.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        static List<Usuario> _listaUsuarios { get; set; } = new List<Usuario>();
+
+
+
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Nuevo(Models.Usuario usuario)
+        {
+            _listaUsuarios.Add(usuario); 
+            return RedirectToAction(nameof(Lista));
+        }
+        public IActionResult Lista()
+        {
+            return View(_listaUsuarios);
+        }
+    }
+}
---
+++ Clase-Layouts-PartialViews-Validaciones/Clase-Layouts-PV-Valid/Clase-Layouts-PV-Valid/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase_Layouts_PV_Valid.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string AvatarURL { get; set; }
+        public string Email { get; set; }
+        public int Puntos { get; set; }
+
+    }
+}
